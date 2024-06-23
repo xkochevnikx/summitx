d00463758e2c23501dc264c654e10b9c9014eb22
svt <xkochevnikx@protonmail.com>
2024-06-23 16:00:26 +0600

    fix(root): upadate

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { _ }) => {
-        config.resolve.alias["@"] = path.join(__dirname, "src");
-        return config;
-    },
     async rewrites() {
         const BASE_API_URL = process.env.BASE_API_URL || "http://localhost:3000";
         return [
