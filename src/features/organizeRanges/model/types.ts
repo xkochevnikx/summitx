b06952517be7978af99d3dbbc2a89b8b08d5cc7f
svt <xkochevnikx@protonmail.com>
2024-06-23 15:31:51 +0600

    test(root): test CI test CI

--- src/features/organizeRanges/model/types.ts
+++ src/features/organizeRanges/model/types.ts
@@ -6,5 +6,4 @@ export interface OrganizedMountainRange extends GeodataMountainRangeResponse {
 
 export interface OrganizedRegions {
     [key: number]: OrganizedMountainRange;
-    sdfsdf: OrganizedMountainRange;
 }
