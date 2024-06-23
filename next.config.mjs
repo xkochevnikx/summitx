d34b96d67814ac08e7bc9670827a3b0b1bca5601
svt <xkochevnikx@protonmail.com>
2024-06-23 15:56:15 +0600

    test(root): ci test

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config, { _ }) => {
+        config.resolve.alias["@"] = path.join(__dirname, "src");
+        return config;
+    },
     async rewrites() {
-        const BASE_API_URL = process.env.BASE_API_URL || 'http://localhost:3000';
+        const BASE_API_URL = process.env.BASE_API_URL || "http://localhost:3000";
         return [
             {
-                source: '/api/:path*',
+                source: "/api/:path*",
                 destination: `${BASE_API_URL}/api/:path*`,
             },
         ];
