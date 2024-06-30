11f9af837d039f3a5b99f444a72ea9aefdf63466
svt <xkochevnikx@protonmail.com>
2024-06-30 18:13:56 +0600

    build(root): telemetry: false

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,7 @@ const nextConfig = {
 
 export default withSentryConfig(nextConfig, {
     authToken: process.env.SENTRY_AUTH_TOKEN,
+    telemetry: false,
     org: "summitx",
     project: "javascript-nextjs",
     enabled: process.env.NODE_ENV === "production",
