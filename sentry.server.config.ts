23949a790072de35dda05dedd055f30fa12e4904
svt <xkochevnikx@protonmail.com>
2024-06-30 18:46:30 +0600

    build(root): remove enabled field

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -3,7 +3,7 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
     environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT,
-    enabled: process.env.NODE_ENV === "production",
+    // enabled: process.env.NODE_ENV === "production",
     tracesSampleRate: 1,
     debug: false,
 });
