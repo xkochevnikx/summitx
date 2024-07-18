e0a530455320f98b15e765dd2dae0c1cd1f6723a
svt <xkochevnikx@protonmail.com>
2024-07-18 07:35:00 +0600

    fix(app): fix imports

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,4 +1,4 @@
-import { OpenLayout } from "./_/layouts/OpenLayout";
+import { OpenLayout } from "./_/layouts/openLayout";
 import { AppLoader } from "./_/loaders/app-loader";
 import { AppProvider } from "./_/providers/app-provider";
 
