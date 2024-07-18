7d3bbbe7be9a010ea2dbe5303e3bd2883bd28807
svt <xkochevnikx@protonmail.com>
2024-07-18 09:09:27 +0600

    build(root): test update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /* eslint-disable no-undef */
 import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config) => {
+        config.resolve.alias["@"] = path.join(__dirname, "src");
+        return config;
+    },
     async rewrites() {
         const BASE_API_URL = process.env.BASE_API_URL || "http://localhost:3000";
         return [
