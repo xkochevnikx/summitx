85970b077bb8bec173f8e0032076e8fa0c5e7a4a
svt <xkochevnikx@protonmail.com>
2024-08-09 10:29:08 +0600

    refactor(shared): update logger config

--- src/shared/lib/logger.ts
+++ src/shared/lib/logger.ts
@@ -1,7 +1,28 @@
 import * as Sentry from "@sentry/nextjs";
 import Pino from "pino";
 
-export const logger = Pino();
+export const logger = Pino({
+    formatters: {
+        level: (label) => {
+            return { level: label.toUpperCase() };
+        },
+        bindings: (bindings) => {
+            return { id: bindings.pid, host: bindings.hostname };
+        },
+    },
+    timestamp: () => {
+        const date = new Date();
+        const formattedDate = date.toLocaleString("ru-RU", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+        });
+        return `,"time":"${formattedDate}"`;
+    },
+});
 
 export const loggedMethod = <A extends any[] = any[], R = any>({
     msg,
